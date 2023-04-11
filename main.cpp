a72d9e5843c6047ba34151d50bf9db8ac9550894
feichay10 <alu0101364544@ull.edu.es>
2023-04-11 20:21:15 +0100

    Updating the menu

--- main.cpp
+++ main.cpp
@@ -1,67 +1,80 @@
+#include <math.h>
+
 #include <QCoreApplication>
 #include <QVector>
 #include <iostream>
-#include <math.h>
 
 class Statistics {
-    // https://stackoverflow.com/questions/7988486/how-do-you-calculate-the-variance-median-and-standard-deviation-in-c-or-java/7988556#7988556
-    QVector<float> data;
-    int size;
+  // https://stackoverflow.com/questions/7988486/how-do-you-calculate-the-variance-median-and-standard-deviation-in-c-or-java/7988556#7988556
+  QVector<float> data;
+  int size;
 
-public:
-    Statistics(QVector<float> data) {
-        this->data = data;
-        size = data.length();
-    }
+ public:
+  Statistics(QVector<float> data) {
+    this->data = data;
+    size = data.length();
+  }
 
-    double getMean() {
-        double sum = 0.0;
-        for(double a : data)
-            sum += a;
-        return sum/size;
-    }
+  double getMean() {
+    double sum = 0.0;
+    for (double a : data) sum += a;
+    return sum / size;
+  }
 
-    double median() {
-       //Arrays.sort(data);
-       std::sort(data.begin(),data.end());
-       if (data.length() % 2 == 0)
-          return (data[(data.length() / 2) - 1] + data[data.length() / 2]) / 2.0;
-       return data[data.length() / 2];
-    }
+  double median() {
+    // Arrays.sort(data);
+    std::sort(data.begin(), data.end());
+    if (data.length() % 2 == 0)
+      return (data[(data.length() / 2) - 1] + data[data.length() / 2]) / 2.0;
+    return data[data.length() / 2];
+  }
 };
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    QVector<float> d;
-    unsigned int total = 60*60*24*365; // sec * min * hour * days
-    int option;
+int main(int argc, char *argv[]) {
+  QCoreApplication a(argc, argv);
+  QVector<float> d;
+  unsigned int total = 60 * 60 * 24 * 365;  // sec * min * hour * days
+  int option;
 
-    std::cout << " ==== Problemas de Sincronización ==== " << std::endl;
-    std::cout << "  1. Secuencialmente." << std::endl;
-    std::cout << "  2. Paralelización con hilos." << std::endl;
+  do {
+    std::cout << " ==== Synchronization problems ==== " << std::endl;
+    std::cout << "  1. Serial mode." << std::endl;
+    std::cout << "  2. Producer-consumer mode." << std::endl;
+    std::cout << "  3. Readers-writer mode." << std::endl;
+    std::cout << "Choose a option: ";
     std::cin >> option;
 
-    if (argc == 1) {
-        for(unsigned int i =0; i < total; i++)
-        {
-            d.push_back(random() % 50 + 50);    // generate a number between 50 and 99
-            if(i % (24*3600) == 0)
-            {
-                Statistics s(d);
-                s.getMean();
-                s.median();
-                std::cout << i << " of " << total << " ";
-                std::cout << "average temperature " << s.getMean() << " ";
-                std::cout << " with median " << s.median() << "\n";
-                d.erase(d.begin(),d.end());
-            }
-        }
-
-        std::cout << "Done in serial mode\n";
-    } else if (argc == 2 && ((std::string)argv[1] == "-h" || ((std::string)argv[1] == "--help"))) {
-        std::cout << "Help guide" << std::endl;
+    if (option < 1 || option > 3) {
+      std::cout << "Invalid option. Try again." << std::endl << std::endl;
     }
+  } while (option < 1 || option > 3);
 
-    return 0;
+  switch (option) {
+    case 1:
+      for (unsigned int i = 0; i < total; i++) {
+        d.push_back(random() % 50 + 50);  // generate a number between 50 and 99
+        if (i % (24 * 3600) == 0) {
+          Statistics s(d);
+          s.getMean();
+          s.median();
+          std::cout << i << " of " << total << " ";
+          std::cout << "average temperature " << s.getMean() << " ";
+          std::cout << " with median " << s.median() << "\n";
+          d.erase(d.begin(), d.end());
+        }
+      }
+      std::cout << "Done in serial mode\n";
+      break;
+    case 2:
+      std::cout << "Done in producer-consumer mode\n";
+      break;
+    case 3:
+      std::cout << "Done in readers-writer mode\n";
+      break;
+    default:
+      std::cout << "Invalid option\n";
+      exit(EXIT_FAILURE);
+      break;
+  }
+  return 0;
 }
