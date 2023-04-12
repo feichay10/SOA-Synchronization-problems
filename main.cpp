1d22c6ba9e803016e6b0516e596dd6ccd3b96154
feichay10 <alu0101364544@ull.edu.es>
2023-04-12 17:27:57 +0100

    Done producer function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <math.h>
-
 #include <QCoreApplication>
 #include <QVector>
 #include <iostream>
+#include <thread>
+#include <QMutex>
+#include <QWaitCondition>
+
+QMutex mutex;
+const unsigned totalBufferSize = 60 * 60 * 24 * 30;
+QWaitCondition buffertNotEmpty;
+QWaitCondition bufferNotFull;
+unsigned comparator = 0;
 
 class Statistics {
   // https://stackoverflow.com/questions/7988486/how-do-you-calculate-the-variance-median-and-standard-deviation-in-c-or-java/7988556#7988556
@@ -46,40 +54,68 @@ void serialMode(QVector<float> d, unsigned int total)
     }
 }
 
-int main(int argc, char *argv[]) {
-  QCoreApplication a(argc, argv);
-  QVector<float> d;
-  unsigned int total = 60 * 60 * 24 * 365;  // sec * min * hour * days
-  int option;
+void producer(QVector<float> d, unsigned int total, unsigned int dataDay)
+{
+    for (unsigned int i = 0; i < total; i++) {
+        mutex.lock();
+        if (comparator == totalBufferSize) {
+            bufferNotFull.wait(&mutex);
+        }
+        mutex.unlock();
 
-  do {
-    std::cout << " ==== Synchronization problems ==== " << std::endl;
-    std::cout << "  1. Serial mode." << std::endl;
-    std::cout << "  2. Producer-consumer mode." << std::endl;
-    std::cout << "  3. Readers-writer mode." << std::endl;
-    std::cout << "Choose a option: ";
-    std::cin >> option;
+        d.push_back(random() % 50 + 50);
+        comparator++;
 
-    if (option < 1 || option > 3) {
-      std::cout << "Invalid option. Try again." << std::endl << std::endl;
+        mutex.lock();
+        if (comparator >= dataDay * 2) {
+            buffertNotEmpty.wakeAll();
+        }
+        mutex.unlock();
     }
-  } while (option < 1 || option > 3);
+}
 
-  switch (option) {
-    case 1:
-        serialMode(d, total);
-        std::cout << "Done in serial mode\n";
-        break;
-    case 2:
-        std::cout << "Done in producer-consumer mode\n";
-        break;
-    case 3:
-        std::cout << "Done in readers-writer mode\n";
-        break;
-    default:
-        std::cout << "Invalid option\n";
-        exit(EXIT_FAILURE);
-        break;
-  }
-  return 0;
+void consumer()
+{
+
+}
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+    QVector<float> d;
+    unsigned int total = 60 * 60 * 24 * 365;  // sec * min * hour * days
+    unsigned int dataDay = 60 * 60 * 24;
+    int option;
+    std::thread p(producer, d, total, dataDay);
+
+    do {
+        std::cout << " ==== Synchronization problems ==== " << std::endl;
+        std::cout << "  1. Serial mode." << std::endl;
+        std::cout << "  2. Producer-consumer mode." << std::endl;
+        std::cout << "  3. Readers-writer mode." << std::endl;
+        std::cout << "Choose a option: ";
+        std::cin >> option;
+
+        if (option < 1 || option > 3) {
+          std::cout << "Invalid option. Try again." << std::endl << std::endl;
+        }
+    } while (option < 1 || option > 3);
+
+      switch (option) {
+        case 1:
+            serialMode(d, total);
+            std::cout << "Done in serial mode\n";
+            break;
+        case 2:
+            p.join();
+            std::cout << "Done in producer-consumer mode\n";
+            break;
+        case 3:
+            std::cout << "Done in readers-writer mode\n";
+            break;
+        default:
+            std::cout << "Invalid option\n";
+            exit(EXIT_FAILURE);
+            break;
+      }
+      return 0;
 }
