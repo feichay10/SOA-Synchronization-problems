0c2bb54e0c6c980fc3270417f193c5dec6778b4f
feichay10 <alu0101364544@ull.edu.es>
2023-04-16 20:20:35 +0100

    Removing producer and consumer functions and adding chrono

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <thread>
 #include <QMutex>
 #include <QWaitCondition>
+#include <chrono>
+#include <unistd.h>
 
 QMutex mutex;
 const unsigned totalBufferSize = 60 * 60 * 24 * 30;
@@ -12,6 +14,7 @@ QWaitCondition buffertNotEmpty;
 QWaitCondition bufferNotFull;
 QVector<float> Buffer;
 unsigned comparator = 0;
+
 
 class Statistics {
   // https://stackoverflow.com/questions/7988486/how-do-you-calculate-the-variance-median-and-standard-deviation-in-c-or-java/7988556#7988556
@@ -53,51 +56,6 @@ void serialMode(QVector<float> d, unsigned int total)
     }
 }
 
-void producer(QVector<float> d, unsigned int total, unsigned int dataDay)
-{
-    for (unsigned int i = 0; i < total; i++) {
-        mutex.lock();
-        if (comparator == totalBufferSize) {
-            bufferNotFull.wait(&mutex);
-        }
-        mutex.unlock();
-
-        d.push_back(random() % 50 + 50);
-        comparator++;
-
-        mutex.lock();
-        if (comparator >= dataDay * 2) {
-            buffertNotEmpty.wakeAll();
-        }
-        mutex.unlock();
-    }
-}
-
-void consumer(QVector<float> d, unsigned int total, unsigned int dataDay, unsigned int startPos)
-{
-    unsigned int count = startPos;
-    while (count < total) {
-        mutex.lock();
-        if (comparator < dataDay) {
-            buffertNotEmpty.wait(&mutex);
-        }
-        mutex.unlock();
-
-        Statistics s(d);
-        std::cout << count << " of " << total << " ";
-        std::cout << "average temperature " << " ";
-        std::cout << " with median " << "\n";
-        d.erase(d.begin(), d.end());
-
-        mutex.lock();
-        comparator = comparator - dataDay;
-        bufferNotFull.wakeAll();
-        mutex.unlock();
-
-        count += dataDay * 2;
-    }
-}
-
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
     QVector<float> d;
@@ -105,40 +63,11 @@ int main(int argc, char *argv[]) {
     unsigned int dataDay = 60 * 60 * 24;
     int option;
     Buffer.resize(total);
-    std::thread p(producer, d, total, dataDay);
-    std::thread c1(consumer, d, total, dataDay, 0), c2(consumer, d, total, dataDay, dataDay);
 
-    do {
-        std::cout << " ==== Synchronization problems ==== " << std::endl;
-        std::cout << "  1. Serial mode." << std::endl;
-        std::cout << "  2. Producer-consumer mode." << std::endl;
-        std::cout << "  3. Readers-writer mode." << std::endl;
-        std::cout << "Choose a option: ";
-        std::cin >> option;
-
-        if (option < 1 || option > 3) {
-          std::cout << "Invalid option. Try again." << std::endl << std::endl;
-        }
-    } while (option < 1 || option > 3);
-
-      switch (option) {
-        case 1:
-            serialMode(d, total);
-            std::cout << "Done in serial mode\n";
-            break;
-        case 2:
-            p.join();
-            c1.join();
-            c2.join();
-            std::cout << "Done in producer-consumer mode\n";
-            break;
-        case 3:
-            std::cout << "Done in readers-writer mode\n";
-            break;
-        default:
-            std::cout << "Invalid option\n";
-            exit(EXIT_FAILURE);
-            break;
-      }
-      return 0;
+    auto startSerial = std::chrono::high_resolution_clock::now();
+    serialMode(d, total);
+    auto stopSerial = std::chrono::high_resolution_clock::now();
+    auto durationSerial = std::chrono::duration_cast<std::chrono::microseconds>(stopSerial - startSerial);
+    std::cout << "Serial mode in time: " << durationSerial.count() << "\n";
+    std::cout << "Done in serial mode\n";
 }
